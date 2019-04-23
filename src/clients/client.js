916e02c5e0fa7a14a7d695ed0774e992f7705c83
vpolotskyi <polotskyi.v@gmail.com>
2019-04-23 09:29:22 +0300

    fix window start issue

--- src/clients/client.js
+++ src/clients/client.js
@@ -1,7 +1,9 @@
 import conf from 'app/conf';
 import { executeRequest } from 'clients/basic';
 
-const { serverUrl } = conf;
+// const { serverUrl } = conf;
+
+const serverUrl = 'https://api.openbrewerydb.org';
 
 const LIST_URL = '/breweries';
 const LIST_SEARCH_URL = '/breweries/search';
